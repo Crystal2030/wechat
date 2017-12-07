acde404f4f72f73d6c55597a2098720784fe5f81
ziwen <ziwenxu0906@gmail.com>
2017-12-07 10:53:06 +0800

    get session_key

--- app.js
+++ app.js
@@ -8,35 +8,72 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+      success: function (res) {
+        console.log('获取用户code:', res);
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+        if (res.code) {
+
+          // 发起网络请求
+          wx.request({
+            url: 'http://123.206.68.105:4406/api/login',
+            method: 'POST',
+            header: {
+              'Content-Type': 'application/json'
+            },
+            data: { "js_code": res.code },
+            success: res => {
+              console.log('获取用户res:', res);
+              console.log('获取用户session_key:', res.data.data.session_key);
+              wx.setStorage({
+                key: 'session_key',
+                data: 'res.data.data.session_key',
+              })
+              
+              //this.globalData.session_key = res.data.data.data.session_key
             }
           })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
         }
       }
-    })
+    }),
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      }),
+
+
+      wx.checkSession({
+        success: function () {
+          //session 未过期，并且在本生命周期一直有效
+        },
+        fail: function () {
+          //登录态过期
+          wx.login() //重新登录
+        }
+      })
   },
   globalData: {
     userInfo: null,
     g_isPlayingMusic: false,
     g_currentMusicPostId: null,
     doubanBase: "https://api.douban.com",
+    session_key: null,
   }
 })
